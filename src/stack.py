14787608b07a1eb0943dc2d79e1ff0f2b8394724
Ehsan Peymani <ehsan-p@innova.no>
2024-10-11 15:29:18 +0200

    some test at office

--- src/stack.py
+++ src/stack.py
@@ -1,22 +1,30 @@
 from typing import List
 
+
 class Stack:
     def __init__(self):
         self._storage: List = []
         self._val = 5
 
-
     def __len__(self):
         return len(self._storage)
 
-    
     def push(self, value: float):
         self._storage.append(value)
-        
-    
+
     def pop(self) -> float:
         try:
             item = self._storage.pop()
         except IndexError:
             item = None
         return item
+
+
+def divide(num1, num2):
+    if num2 == 0:
+        raise ValueError("Value cannot be zero")
+    return num1 / num2
+
+
+def divide2(num1, num2):
+    return num1 / num2
--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.stack import Stack
+from src.stack import Stack, divide
 
 
 @pytest.fixture
@@ -16,17 +16,28 @@ def test_constructor(stack):
 def test_push(stack):
     stack.push(3)
     assert len(stack) == 1
-    
+
     stack.push(2)
-    assert len(stack) == 2  
-    
-    
+    assert len(stack) == 2
+
+
 def test_pop(stack):
     stack.push(3)
     assert stack.pop() == 3
-    
+
 
 @pytest.mark.smoke
 def test_pop_empty_stack():
     s = Stack()
-    assert s.pop() == None 
+    assert s.pop() == None
+
+
+def test_divide_by_zero():
+    with pytest.raises(Exception):
+        print("ehsan peymani", end=" ")
+        divide(5, 0)
+        
+
+@pytest.mark.xfail
+def test_not_implemented():
+    assert False
